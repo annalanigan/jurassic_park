08fbcfd4ba8c9de6b8dd1427d8618e94fa7e07bb
annalanigan <annalanigan@outlook.com>
2018-01-23 15:29:20 +0000

    initial commit - dinosaur has a name test passed

---
+++ dinosaur.js
@@ -0,0 +1,6 @@
+const Dinosaur = function(name, eggs){
+  this.name = name;
+  this.offspring = eggs;
+}
+
+module.exports = Dinosaur;
---
+++ specs/dinosaur_specs.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const Dinosaur = require('../dinosaur.js');
+
+describe('dinosaur', function(){
+
+  var dinosaur1;
+  var dinosaur2;
+  var dinosaur3;
+
+  beforeEach(function(){
+    dinosaur1 = new Dinosaur("T-Rex", 3);
+    dinosaur2 = new Dinosaur("Triceratops", 4);
+    dinosaur3 = new Dinosaur("Argentinosaurus", 1);
+  })
+
+  it('should have a name', function(){
+    assert.strictEqual(dinosaur1.name, "T-Rex");
+  })
+
+})
